5e7ef1c9795f93030ede60cc2ed288451e64ca7c
Paul Guaman <guaman.bryan.paul@gmail.com>
2020-12-10 11:17:41 -0500

    final ejercicio 3

--- src/main/java/com/clearminds/impl/TestManager.java
+++ src/main/java/com/clearminds/impl/TestManager.java
@@ -5,7 +5,7 @@ import com.clearminds.model.Persona;
 
 public class TestManager {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 		// TODO Auto-generated method stub
 		PersonaManager per = new PersonaManager();
 		per.insertarPersona(new Persona("paul", "guaman", "121232"));
--- src/main/java/com/clearminds/interfaces/PersonaManager.java
+++ src/main/java/com/clearminds/interfaces/PersonaManager.java
@@ -1,12 +1,36 @@
 package com.clearminds.interfaces;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
+
 import com.clearminds.model.Persona;
 
 public class PersonaManager {
 	ServicioPersona srv;
-	public PersonaManager() {
-		ServicioPersonaBdd srvbbd= new ServicioPersonaBdd();
-		srv=srvbbd;
+	String res;
+	public  String leerPropiedad(String propiedad) {
+		File f=new File("conexion.properties");
+//		System.out.println("ruta:"+f.getAbsoluteFile());
+		try {
+
+			FileReader reader = new FileReader("config.properties");
+			Properties p = new Properties();
+			p.load(reader);
+			res= (p.getProperty(propiedad));
+			return res;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+	}
+	
+	
+	public PersonaManager() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		String var=leerPropiedad("archivo");
+		Class<?> clase =Class.forName(var);
+		srv=(ServicioPersona)clase.newInstance();
+
 	}
 	
 	public void insertarPersona(Persona persona) {
